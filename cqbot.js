42da640e8e7de7fdf5dc4d451dd572be53ea073b
takayama <takayama@foxmail.com>
2020-03-15 17:20:11 +0900

    forbidden this

--- cqbot.js
+++ cqbot.js
@@ -91,6 +91,9 @@ class Session {
         ws.send(JSON.stringify(res))
     }
     receive(data) {
+        if (data.message.includes("this") && data.user_id !== master) {
+            return
+        }
         if (data.message.includes("constructor")) {
             this._send('由于constructor有bug可在沙盒内操纵父进程，已被禁用。')
             return
