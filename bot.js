f7b076e5f5cd9af8afbc18e8059f1a703813f6d5
takayama <takayama@foxmail.com>
2020-04-25 00:52:55 +0900

    重构 增加cqhttp类

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 const blacklist = [3507349275,429245111]
 const owner = 372914165
 const master = []
@@ -8,177 +8,115 @@ const isMaster = (uid)=>{
 const reboot = ()=>{
     process.exit(1)
 }
-const sessions = {
-    "private": {},
-    "group": {},
-    "discuss": {}
-}
-let ws = null
 
 // 固定指令
-const commands = require('./commands')
+const commands = require("./commands")
 
 // 沙盒初始化
 const sandbox = require("./utils/sandbox")
-sandbox.require("向听", require('syanten'))
+sandbox.require("向听", require("syanten"))
 let timeout = 50
 
 // 敏感词
-const ero = require('./ero')
+const ero = require("./ero")
 
-const main = (conn, data)=>{
-    ws = conn
-    data = JSON.parse(data)
-    if (data.post_type === "message") {
-        if (!sessions[data.message_type]) return
-        if (data.message_type === "private") {
-            data.private_id = data.user_id
+const replyFilter = (msg)=>{
+    if (typeof msg === "undefined")
+        return
+    if (typeof msg === "function")
+        msg = `[Function: ${msg.name?msg.name:"anonymous"}]`
+    if (typeof msg === "object") {
+        try {
+            msg = JSON.stringify(msg)
+        } catch (e) {
+            msg = "对象过大无法保存，将被丢弃。"
         }
-        if (!sessions[data.message_type][data[data.message_type + "_id"]]) {
-            sessions[data.message_type][data[data.message_type + "_id"]] = new Session(data)
-        }
-        let session = sessions[data.message_type][data[data.message_type + "_id"]]
-        let message = ""
-        for (let v of data.message) {
-            if (v.type === "text")
-                message += v.data.text
-            else if (v.type) {
-                if (v.type === 'at')
-                    message += "\""
-                message += "[CQ:" + v.type
-                for (let k in v.data)
-                    message += `,${k}=${v.data[k]}`
-                message += "]"
-                if (v.type === 'at')
-                    message += "\""
-            }
-        }
-        data.message = message
-        session.onMessage(data)
+    } else if (typeof msg !== "string") {
+        msg = msg.toString()
     }
-    if (data.post_type === "request") {
-        if (data.request_type === "friend") {
-            ws.send(JSON.stringify({
-                "action": "set_friend_add_request",
-                "params": {
-                    "flag": data.flag
-                }
-            }))
-        }
-        if (data.request_type === "group" && data.sub_type === "invite") {
-            ws.send(JSON.stringify({
-                "action": "set_group_add_request",
-                "params": {
-                    "flag": data.flag,
-                    "sub_type": "invite"
-                }
-            }))
-        }
-    }
-    if (data.post_type === "notice") {
-        if (data.notice_type === "group_ban" && data.sub_type === "ban") {
-            if (data.user_id === data.self_id && data.duration > 86400)
-                ws.send(JSON.stringify({
-                    "action": "set_group_leave",
-                    "params": {
-                        "group_id": data.group_id
-                    }
-                }))
-        }
-    }
-}
-
-class Session {
-    constructor(data) {
-        this.action = "send_" + data.message_type + "_msg"
-        for (let i in data) {
-            this[i] = data[i]
-        }
-    }
-    _send(msg) {
-        if (typeof msg === 'undefined')
+    if (typeof msg === "string") {
+        msg = msg.replace(ero, "**")
+        if (msg.length > 4500)
+            msg = msg.substr(0, 4495) + "\n..."
+        if (!msg.length)
             return
-        if (typeof msg === 'function')
-            msg = `[Function: ${msg.name?msg.name:'anonymous'}]`
-        if (typeof msg === "object") {
-            try {
-                msg = JSON.stringify(msg)
-            } catch (e) {
-                msg = "对象过大无法保存，将被丢弃。"
-            }
-        } else if (typeof msg !== 'string') {
-            msg = msg.toString()
-        }
-        if (typeof msg === 'string') {
-            msg = msg.replace(ero, '**')
-            if (msg.length > 4500)
-                msg = msg.substr(0, 4495) + "\n..."
-            if (!msg.length)
-                return
-        }
-        let res = {
-            "action": this.action,
-            "params": {
-                "user_id": this.user_id,
-                "group_id": this.group_id,
-                "discuss_id": this.discuss_id,
-                "message": msg
-            }
-        }
-        ws.send(JSON.stringify(res))
     }
-    async onMessage(data) {
-        data.message = data.message.trim()
-        let prefix = data.message.substr(0, 1)
-        if (prefix === "!") return
-        if (prefix === "-") {
-            let split = data.message.substr(1).trim().split(" ")
-            let command = split.shift()
-            let param = split.join(" ")
-            if (isMaster(data.user_id) && command === "raw" && param.length)
-                return ws.send(param)
-            if (command === '获得管理') {
-                return ws.send(JSON.stringify({
-                    "action": "set_group_admin",
-                    "params": {
-                        "user_id": data.user_id,
-                        "group_id": data.group_id,
-                        "enable": true
-                    }
-                }))
-            }
-            if (command === '放弃管理') {
-                return ws.send(JSON.stringify({
-                    "action": "set_group_admin",
-                    "params": {
-                        "user_id": data.user_id,
-                        "group_id": data.group_id,
-                        "enable": false
-                    }
-                }))
-            }
-            if (isMaster(data.user_id) && command === "run" && param.length) {
-                let result
-                try {
-                    result = eval(data.message.substr(5))
-                } catch(e) {
-                    result = e.stack
-                }
-                return this._send(result)
-            }
-            if (command === '龙王')
-                param = data.group_id
-            if (command === '发言')
-                param = [data.group_id, data.user_id]
-            if (commands.hasOwnProperty(command)) {
-                return this._send(await commands[command](param))
-            }
-        } else {
-            if (blacklist.includes(data.user_id))
-                return
-            return this._send(sandbox.run(data, timeout, isMaster(data.user_id)))
-        }
-    }
+    return msg
 }
 
-module.exports = main
+const CQHttp = require("./cqhttp")
+const bot = new CQHttp()
+
+bot.on("request.friend", bot.approve)
+bot.on("request.group.invite", bot.approve)
+bot.on("notice.group_ban.ban", (data)=>{
+    if (data.user_id === data.self_id && data.duration > 86400)
+        bot.setGroupLeave(data.group_id)
+})
+bot.on("message", async(data)=>{
+    const reply = (msg)=>{
+        if (typeof replyFilter(msg) === "string")
+            bot.reply(data, msg, {at_sender: false})
+    }
+    let message = ""
+    for (let v of data.message) {
+        if (v.type === "text")
+            message += v.data.text
+        else if (v.type) {
+            if (v.type === "at")
+                message += "\""
+            message += "[CQ:" + v.type
+            for (let k in v.data)
+                message += `,${k}=${v.data[k]}`
+            message += "]"
+            if (v.type === "at")
+                message += "\""
+        }
+    }
+    data.message = message.trim()
+    let prefix = data.message.substr(0, 1)
+    if (prefix === "!") return
+    if (prefix === "-") {
+        let split = data.message.substr(1).trim().split(" ")
+        let command = split.shift()
+        let param = split.join(" ")
+        if (command === "获得管理") {
+            return bot.setGroupAdmin(data.group_id, data.user_id)
+        }
+        if (command === "放弃管理") {
+            return bot.setGroupAdmin(data.group_id, data.user_id, false)
+        }
+        if (isMaster(data.user_id) && command === "request" && param.length) {
+            let params = param.split(" ")
+            let action = params.shift()
+            if (bot.hasOwnProperty(action) && typeof bot[action] === 'function')
+                reply(await bot[action].apply(null, params))
+            return
+        }
+        if (isMaster(data.user_id) && command === "run" && param.length) {
+            let result
+            try {
+                result = eval(data.message.substr(5))
+            } catch(e) {
+                result = e.stack
+            }
+            return reply(result)
+        }
+        if (command === "龙王")
+            param = data.group_id
+        if (command === "发言")
+            param = [data.group_id, data.user_id]
+        if (commands.hasOwnProperty(command)) {
+            return reply(await commands[command](param))
+        }
+    } else {
+        if (blacklist.includes(data.user_id))
+            return
+        return reply(sandbox.run(data, timeout, isMaster(data.user_id)))
+    }
+})
+
+module.exports = (conn, data)=>{
+    bot.conn = conn
+    bot.onEvent(data)
+}
---
+++ cqhttp.js
@@ -0,0 +1,301 @@
+'use strict'
+const Events = require('events')
+/**
+ * implement of QQBot
+ */
+class CQHttp extends Events {
+    constructor({qq = undefined, timeout = 5000} = {}) {
+        super()
+        this.qq = qq
+        this.timeout = timeout
+        this._conn = null
+        this.queue = {}
+    }
+    set conn(conn) {
+        this._conn = conn
+    }
+    onEvent(data) {
+        console.log("receive: ", data)
+        data = JSON.parse(data)
+        if (this.qq && data.self_id != this.qq)
+            return
+        switch (data.post_type) {
+            case 'request':
+            case 'notice':
+            case 'message':
+                let event = data.post_type
+                this.emit(event, data)
+                event += '.' + data[event+'_type']
+                this.emit(event, data)
+                if (data.sub_type) {
+                    event += '.' + data.sub_type
+                    this.emit(event, data)
+                }
+                break
+            default:
+                if (data.echo && this.queue.hasOwnProperty(data.echo)) {
+                    this.queue[data.echo](data)
+                    delete this.queue[data.echo]
+                } else {
+                    this.emit('unhandled', data)
+                }
+                break
+        }
+    }
+    async sendPrivateMsg(uid, msg, escape = false) {
+        let data = this._buildData('send_private_msg', {
+            user_id: uid,
+            message: msg,
+            auto_escape: escape
+        })
+        return await this._request(data)
+    }
+    async sendGroupMsg(gid, msg, escape = false) {
+        let data = this._buildData('send_group_msg', {
+            group_id: gid,
+            message: msg,
+            auto_escape: escape
+        })
+        return await this._request(data)
+    }
+    async sendDiscussMsg(did, msg, escape = false) {
+        let data = this._buildData('send_discuss_msg', {
+            discuss_id: did,
+            message: msg,
+            auto_escape: escape
+        })
+        return await this._request(data)
+    }
+    // async sendMsg(type, id, msg, escape = false) {
+    // 不实现该方法
+    // }
+    deleteMsg(mid) {
+        let data = this._buildData('delete_msg', {
+            message_id: mid
+        })
+        this._send(data)
+    }
+    sendLike(uid, times = 10) {
+        let data = this._buildData('send_like', {
+            user_id: uid,
+            times: times
+        })
+        this._send(data)
+    }
+    setGroupKick(gid, uid, forever = false) {
+        let data = this._buildData('set_group_kick', {
+            group_id: gid,
+            user_id: uid,
+            reject_add_request: forever
+        })
+        this._send(data)
+    }
+    setGroupBan(gid, uid, duration = 600) {
+        let data = this._buildData('set_group_ban', {
+            group_id: gid,
+            user_id: uid,
+            duration: duration
+        })
+        this._send(data)
+    }
+    setGroupAnonymousBan(gid, flag, duration = 600) {
+        let data = this._buildData('set_group_anonymous_ban', {
+            group_id: gid,
+            flag: flag,
+            duration: duration
+        })
+        this._send(data)
+    }
+    setGroupWholeBan(gid, enable = true) {
+        let data = this._buildData('set_group_whole_ban', {
+            group_id: gid,
+            enable: enable
+        })
+        this._send(data)
+    }
+    setGroupAdmin(gid, uid, enable = true) {
+        let data = this._buildData('set_group_admin', {
+            group_id: gid,
+            user_id: uid,
+            enable: enable
+        })
+        this._send(data)
+    }
+    setGroupAnonymous(gid) {
+        let data = this._buildData('set_group_anonymous', {
+            group_id: gid,
+            enable: enable
+        })
+        this._send(data)
+    }
+    setGroupCard(gid, uid, card = undefined) {
+        let data = this._buildData('set_group_card', {
+            group_id: gid,
+            user_id: uid,
+            card: card
+        })
+        this._send(data)
+    }
+    setGroupLeave(gid, dismiss = false) {
+        let data = this._buildData('set_group_leave', {
+            group_id: gid,
+            is_dismiss: dismiss
+        })
+        this._send(data)
+    }
+    setGroupSpecialTitle(gid, uid, title, duration = -1) {
+        let data = this._buildData('set_group_special_title', {
+            group_id: gid,
+            user_id: uid,
+            special_title: title,
+            duration: duration
+        })
+        this._send(data)
+    }
+    setDiscussLeave(did) {
+        let data = this._buildData('set_discuss_leave', {
+            discuss_id: did
+        })
+        this._send(data)
+    }
+    setFriendAddRequest(flag, approve = true, remark = undefined) {
+        let data = this._buildData('set_friend_add_request', {
+            flag: flag,
+            approve: approve,
+            remark: remark
+        })
+        this._send(data)
+    }
+    // setGroupAddRequest(flag, type, approve, reason) {
+    // 不实现该方法
+    // }
+    setGroupRequest(flag, approve = true, reason = undefined) {
+        let data = this._buildData('set_group_add_request', {
+            flag: flag,
+            type: 'add',
+            approve: approve,
+            reason: reason
+        })
+        this._send(data)
+    }
+    setGroupInvitation(flag, approve, reason) {
+        let data = this._buildData('set_group_add_request', {
+            flag: flag,
+            type: 'invite',
+            approve: approve,
+            reason: reason
+        })
+        this._send(data)
+    }
+    sendGroupNotice(gid, title, content) {
+        let data = this._buildData('_send_group_notice', {
+            group_id: gid,
+            title: title,
+            content: content
+        })
+        this._send(data)
+    }
+    async getLoginInfo() {
+        let data = this._buildData('get_login_info')
+        return await this._request(data)
+    }
+    async getFriendList() {
+        let data = this._buildData('get_friend_list')
+        return await this._request(data)
+    }
+    async getGroupList() {
+        let data = this._buildData('get_group_list')
+        return await this._request(data)
+    }
+    async getGroupInfo(gid, cache = true) {
+        let data = this._buildData('get_group_info', {
+            group_id: gid,
+            no_cache: !cache
+        })
+        return await this._request(data)
+    }
+    async getGroupMemberInfo(gid, uid, cache = true) {
+        let data = this._buildData('get_group_member_info', {
+            group_id: gid,
+            user_id: uid,
+            no_cache: !cache
+        })
+        return await this._request(data)
+    }
+    async getGroupMemberList(gid) {
+        let data = this._buildData('get_group_member_list', {
+            group_id: gid
+        })
+        return await this._request(data)
+    }
+    async getGroupNotice(gid) {
+        let data = this._buildData('_get_group_notice', {
+            group_id: gid
+        })
+        return await this._request(data)
+    }
+    async getVipInfo(uid) {
+        let data = this._buildData('_get_vip_info', {
+            user_id: uid
+        })
+        return await this._request(data)
+    }
+    async getStatus() {
+        let data = this._buildData('get_status')
+        return await this._request(data)
+    }
+    approve(origin, approve = true, remark = undefined) {
+        let data = this._buildData('.handle_quick_operation', {
+            context: origin,
+            operation: {
+                approve: approve,
+                remark: remark,
+                reason: remark
+            }
+        })
+        this._send(data)
+    }
+    reply(origin, msg, option = {}) {
+        let data = this._buildData('.handle_quick_operation', {
+            context: origin,
+            operation: {
+                reply: msg,
+                ...option
+            }
+        })
+        this._send(data)
+    }
+    _buildData(action, params = {}) {
+        return {
+            action: action,
+            params: params
+        }
+    }
+    _send(data) {
+        console.log("no conn")
+        if (!this._conn)
+            return false
+        console.log("send: ", data)
+        this._conn.send(JSON.stringify(data))
+        return true
+    }
+    async _request(data) {
+        return new Promise((resolve, reject)=>{
+            data.echo = Math.random() * Date.now()
+            if (this._send(data)) {
+                let id = setTimeout(()=>{
+                    delete this.queue[data.echo]
+                    resolve({error: 'timeout'})
+                }, this.timeout)
+                this.queue[data.echo] = (data)=>{
+                    clearTimeout(id)
+                    resolve(data)
+                }
+            } else {
+                resolve({error: 'connection error'})
+            }
+        })
+    }
+}
+
+module.exports = CQHttp
