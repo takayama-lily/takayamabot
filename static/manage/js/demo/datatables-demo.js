2c9c2f3987214a8af9f64275fabd7a100c744573
takayama <takayama@foxmail.com>
2020-04-30 00:42:24 +0900

    1

--- static/manage/js/demo/datatables-demo.js
+++ static/manage/js/demo/datatables-demo.js
@@ -1,4 +1,16 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  fetch('/manage/bot') 
+    .then(function(res) { 
+        res.json().then(function(data) {
+          let html = `<tr><th>key</th><th>value</th></tr>`
+            for (let k in data) {
+              if (typeof data[k] === 'string') {
+                let html = `<tr><td>${k}</td><td>${data[k]}</td></tr>`
+                $("#dataTable tbody").append(html)
+              }
+            }
+            $('#dataTable').DataTable()
+        })
+    })
 });
