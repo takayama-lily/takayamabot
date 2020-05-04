558fb8ed216919c330d20ae63204d821c237aa27
takayama <takayama@foxmail.com>
2020-05-05 00:56:01 +0900

    #16

--- bot.js
+++ bot.js
@@ -64,7 +64,9 @@ bot.on("notice.group_increase", async(data)=>{
     }
 })
 bot.on("message", async(data)=>{
-    if (blacklist.includes(data.user_id))
+    let uid = data.user_id
+    let gid = data.group_id
+    if (blacklist.includes(uid))
         return
     const reply = (msg)=>{
         msg = replyFilter(msg)
@@ -72,35 +74,20 @@ bot.on("message", async(data)=>{
             bot.reply(data, msg, {at_sender: false})
         }
     }
-    let message = ""
-    for (let v of data.message) {
-        if (v.type === "text")
-            message += v.data.text
-        else if (v.type) {
-            if (v.type === "at")
-                message += "'"
-            message += "[CQ:" + v.type
-            for (let k in v.data)
-                message += `,${k}=${v.data[k]}`
-            message += "]"
-            if (v.type === "at")
-                message += "'"
-        }
-    }
-    data.message = message.trim()
-    let prefix = data.message.substr(0, 1)
+    let message = data.raw_message.trim()
+    let prefix = message.substr(0, 1)
     if (prefix === "!") return
     if (prefix === "-") {
-        let split = data.message.substr(1).trim().split(" ")
+        let split = message.substr(1).trim().split(" ")
         let command = split.shift()
         let param = split.join(" ")
         if (command === "获得管理") {
-            return bot.setGroupAdmin(data.group_id, data.user_id)
+            return bot.setGroupAdmin(gid, uid)
         }
         if (command === "放弃管理") {
-            return bot.setGroupAdmin(data.group_id, data.user_id, false)
+            return bot.setGroupAdmin(gid, uid, false)
         }
-        if (isMaster(data.user_id) && command === "request" && param.length) {
+        if (isMaster(uid) && command === "request" && param.length) {
             let params = param.split(" ")
             let action = params.shift()
             if (typeof bot[action] === 'function') {
@@ -108,30 +95,30 @@ bot.on("message", async(data)=>{
             }
             return
         }
-        if (isMaster(data.user_id) && command === "run" && param.length) {
+        if (isMaster(uid) && command === "run" && param.length) {
             let result
             try {
-                result = eval(data.message.substr(5))
+                result = eval(message.substr(5))
             } catch(e) {
                 result = e.stack
             }
             return reply(result)
         }
         if (command === "龙王")
-            param = data.group_id
+            param = gid
         if (command === "发言")
-            param = [data.group_id, data.user_id]
+            param = [gid, uid]
         if (commands.hasOwnProperty(command)) {
             return reply(await commands[command](param))
         }
     } else {
-        let res = sandbox.run(data, timeout, isMaster(data.user_id))
-        if (data.group_id) {
+        let res = sandbox.run(data, timeout, isMaster(uid))
+        if (gid) {
             const now = Date.now()
-            if (fff[data.group_id] && now - fff[data.group_id] <= fff.limit)
+            if (fff[gid] && now - fff[gid] <= fff.limit)
                 return
             if (res !== undefined && res !== "")
-                fff[data.group_id] = now
+                fff[gid] = now
         }
         return reply(res)
     }
