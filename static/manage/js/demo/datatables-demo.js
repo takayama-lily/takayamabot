cf9e4552574e765eac2be25c6ea2fdf5942e7666
takayama <takayama@foxmail.com>
2020-05-10 08:54:47 +0900

    图片转链接

--- static/manage/js/demo/datatables-demo.js
+++ static/manage/js/demo/datatables-demo.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
           let html = `<tr><th>key</th><th>value</th></tr>`
             for (let k in data) {
               if (typeof data[k] === 'string') {
+                data[k] = data[k].replace(/(https?:\/\/){1}.+[^\]]/g, function(a){
+                  return `<a target="_blank" href="${a}">图片</a>`
+                })
                 let html = `<tr><td>${k}</td><td>${data[k]}</td></tr>`
                 $("#dataTable tbody").append(html)
               }
