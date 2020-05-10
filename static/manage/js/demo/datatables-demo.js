617fbea17d286d28de5fb05cb163e59d27d8f2b6
takayama <takayama@foxmail.com>
2020-05-10 09:02:06 +0900

    1

--- static/manage/js/demo/datatables-demo.js
+++ static/manage/js/demo/datatables-demo.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             for (let k in data) {
               if (typeof data[k] === 'string') {
                 data[k] = data[k].replace(/(https?:\/\/){1}.+[^\]]/g, function(a){
-                  return `<a target="_blank" href="${a}">图片</a>`
+                  return `<a target="_blank"href="${a}">图片</a>`
                 })
                 let html = `<tr><td>${k}</td><td>${data[k]}</td></tr>`
                 $("#dataTable tbody").append(html)
