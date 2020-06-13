60985fda2d3a137f6fccf30f3400f4b171e7a224
takayama <takayama@foxmail.com>
2020-06-13 23:06:28 +0900

    文言

--- commands.js
+++ commands.js
@@ -13,9 +13,9 @@ const db = new sqlite3.Database('/var/www/db/eventv2.db')
 const commands = {
     "qh": async function(param) {
         if (!param.length)
-            return "需要输入用户ID。"
+            return "需要加上玩家ID。"
         if (isNaN(param))
-            return '现在无法使用用户名，只能使用ID来找到用户。'
+            return '暂时无法使用昵称，只能使用ID来找到玩家。'
         return await mjutil.shuibiao(param)
     },
     "qhcn": async function(param) {
@@ -23,9 +23,9 @@ const commands = {
     },
     "qhjp": async function(param) {
         if (!param.length)
-            return "需要输入用户ID。"
+            return "需要加上玩家ID。"
         if (isNaN(param))
-            return '现在无法使用用户名，只能使用ID来找到用户。'
+            return '暂时无法使用昵称，只能使用ID来找到玩家。'
         // if (!param.length)
         //     return "没有输入用户名。输入例:\n-qhjp 千羽黒乃"
         else
@@ -42,7 +42,7 @@ const commands = {
     },
     "anime": async function(param) {
         if (!param.length)
-            return "没有输入名称。输入例:\n-动漫 公主连结"
+            return "没有输入名称。用法例:\n-动漫 公主连结"
         else
           return await bgm.getBangumi("anime", param)
     },
@@ -76,27 +76,27 @@ const commands = {
     "pl": async function(param) {
         param = param.trim()
         if (!param) {
-            let s = `-----牌理指令紹介-----
-自摸例: -pl 111m234p567s1122z2z
-栄和例: -pl 111m234p567s1122z+2z
-★mpsz=萬筒索字 1-7z=東南西北白發中 0=赤
-★未和牌的时候会自动计算向听数牌理
-★查看高级用法输入: -pl 高级`
+            let s = `-----牌理使用方法-----
+自摸输入: -pl 111m234p567s1122z2z
+栄和输入: -pl 111m234p567s1122z+2z
+※mpsz=萬筒索字 1-7z=東南西北白發中 0=赤
+※未和牌的时候会自动计算向听数牌理
+※查看高级用法输入: -pl 高级`
             return s
         }
         if (param === '高级') {
-            let s = `-----牌理指令紹介-----
-★副露&dora "-pl 33m+456p99s6666z777z+d56z"
-※副露: 456p順子、9s暗槓、発明槓、中明刻 / dora: 白発
-★付属役 "-pl 11123456789999m+rih21"
-※付属役: 立直一発海底(南場東家)
-★付属役一覧
+            let s = `-----牌理高级用法-----
+● 副露和dora输入: -pl 33m+456p99s6666z777z+d56z
+※456p順子、9s暗槓、6z明槓、7z明刻 / 5z6z为dora
+● 付属役输入: -pl 11123456789999m+rih21
+※立直一発海底(南場東家)
+● 付属役一覧
 t=天和/地和/人和
 w=w立直  l(r)=立直  y(i)=一発
 h=海底/河底  k=槍槓/嶺上
 o=古役有効 (目前只有人和,大七星)
-★場風自風設定 (default: 東場南家)
-1=11=東場東家  2=12=東場南家  3=13=東場西家  4=14=東場北家
+● 場風自風設定 (default: 東場南家)
+11=東場東家  12=東場南家  13=東場西家  14=東場北家
 21=南場東家  22=南場南家  23=南場西家  24=南場北家
 -----Code Github-----
 https://github.com/takayama-lily/riichi`
