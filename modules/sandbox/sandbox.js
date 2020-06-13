aca6c2d96bd192f6d0eb61da941469ea01b5cf44
takayama <takayama@foxmail.com>
2020-06-13 16:16:54 +0900

    rename

--- modules/sandbox/sandbox.js
+++ modules/sandbox/sandbox.js
@@ -1,10 +1,10 @@
 const fs = require("fs")
 const path = require("path")
 const vm = require("vm")
-const dataPath = module.path + "/../data/"//path.join(__dirname, "data")
+const dataPath = path.join(__dirname, "data")
 const contextFile = path.join(dataPath, "context")
 const fnFile = path.join(dataPath, "context.fn")
-const initCodeFile = path.join(module.path, "sandbox.code.js")
+const initCodeFile = path.join(__dirname, "sandbox.code.js")
 if (!fs.existsSync(dataPath)) {
     fs.mkdirSync(dataPath, {recursive: true, mode: 0o700})
 }
