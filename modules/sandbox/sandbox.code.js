5375c1903034a6d8239c683ab6b4865ac1cffa47
takayama <takayama@foxmail.com>
2020-06-20 04:47:10 +0900

    debug

--- modules/sandbox/sandbox.code.js
+++ modules/sandbox/sandbox.code.js
@@ -238,52 +238,56 @@ Object.defineProperty(this, "database", {
 	value: this.database
 })
 
+this.set_history_allowed = false
 this.set_history = this.set_history && typeof this.set_history === "object" ? this.set_history : {}
-// this.set_history = new Proxy(this.set_history, {
-// 	set: (o, k, v, r)=>{
-// 		if (!this.set_history_allowed)
-// 			throw error403
-// 		return Reflect.set(o, k, v)
-// 	},
-// 	has: (o, k)=>{
-// 		throw error403
-// 	},
-// 	deleteProperty: (o, k)=>{
-// 		throw error403
-// 	},
-// 	defineProperty: (o, k, d)=>{
-// 		throw error403
-// 	},
-// 	ownKeys: (o)=>{
-// 		if (!qq())
-// 			return Reflect.ownKeys(o)
-// 		throw error403
-// 	},
-// 	preventExtensions: (o)=>{
-// 		throw error403
-// 	},
-// 	setPrototypeOf: (o, prototype)=>{
-// 		throw error403
-// 	}
-// })
-// Object.defineProperty(this, "set_history", {
-// 	configurable: false,
-// 	enumerable: true,
-// 	writable: false,
-// 	value: this.set_history
-// })
+this.set_history = new Proxy(this.set_history, {
+	set: (o, k, v, r)=>{
+		if (!this.set_history_allowed)
+			throw error403
+		return Reflect.set(o, k, v)
+	},
+	has: (o, k)=>{
+		throw error403
+	},
+	deleteProperty: (o, k)=>{
+		throw error403
+	},
+	defineProperty: (o, k, d)=>{
+		throw error403
+	},
+	ownKeys: (o)=>{
+		if (!qq())
+			return Reflect.ownKeys(o)
+		throw error403
+	},
+	preventExtensions: (o)=>{
+		throw error403
+	},
+	setPrototypeOf: (o, prototype)=>{
+		throw error403
+	}
+})
+Object.defineProperty(this, "set_history", {
+	configurable: false,
+	enumerable: true,
+	writable: false,
+	value: this.set_history
+})
 Object.defineProperty(this, "recordSetHistory", {
 	configurable: false,
 	enumerable: false,
 	writable: false,
 	value: (k)=>{
-		if (k !== "data" && typeof this.qq === "function" && this.qq())
+		if (k !== "data" && typeof this.qq === "function" && this.qq()) {
+			this.set_history_allowed = true
 			this.set_history[k] = {
 				id: this.qq(),
 				name: this.user(0),
 				group: this.qun(),
 				card: this.user(1)
 			}
+			this.set_history_allowed = false
+		}
 	}
 })
 delete Proxy
