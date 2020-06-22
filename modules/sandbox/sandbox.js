7f0a451a8869e38fe33a737d0464777077b3810b
takayama <takayama@foxmail.com>
2020-06-22 15:56:06 +0900

    群名

--- modules/sandbox/sandbox.js
+++ modules/sandbox/sandbox.js
@@ -166,7 +166,9 @@ module.exports.run = (code, isAdmin = false)=>{
 //设置环境变量
 const setEnv = (env = {})=>{
     set_env_allowed = true
-    vm.runInContext(`this.data=data=${JSON.stringify(env)};Object.freeze(this.data);Object.freeze(this.data.sender);Object.freeze(this.data.anonymous);`, context)
+    vm.runInContext(`this.data=data=` + JSON.stringify(env), context)
+    vm.runInContext(`if ($.getGroupInfo()) this.data.group_name=$.getGroupInfo().group_name`, context)
+    vm.runInContext(`Object.freeze(this.data);Object.freeze(this.data.sender);Object.freeze(this.data.anonymous);`, context)
     set_env_allowed = false
 }
 module.exports.setEnv = setEnv
