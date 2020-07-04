71954d22dd275f849e2689558e7e0d51188b0178
takayama <takayama@foxmail.com>
2020-07-05 06:58:20 +0900

    文言

--- api_passon.js
+++ api_passon.js
@@ -3,22 +3,22 @@ const https = require("https")
 const sandbox = require("./modules/sandbox/sandbox")
 
 const $ = sandbox.run(`new String(\`这是一个云JavaScript环境。聊天窗口就是控制台。
-程序会读取用户发送消息并尝试作为JS代码执行，如果有结果则返回，出错则丢弃。
+程序会读取用户发送的消息并尝试作为JS代码执行, 如果有结果则返回, 出错则丢弃。
 
 该文档可能需要一定的编程基础才能充分理解。
-该文档默认你会使用JavaScript，或其他类似语言。
+该文档默认你会使用JavaScript, 或其他类似语言。
 准备好了就输入$.help或$.doc查看文档吧！\`)`)
 
 $.help = $.doc = `● 前言
 请勿设置任何可能导致封号的敏感违规内容。
-调式手段是在代码前加上反斜杠"\\"，这样会输出错误信息。
+调式手段是在代码前加上反斜杠"\\", 这样会输出错误信息。
 支持使用CQ码定义图片等消息。
-环境内只包含ECMAScript6原生API，和一些增强API，不要尝试使用浏览器或nodejs的特有API。
-由于安全原因，无法使用this、async、const关键字，并且不支持eval和Promise异步操作。
+环境内只包含ECMAScript6原生API, 和一些增强API, 不要尝试使用浏览器或nodejs的特有API。
+由于安全原因, 无法使用this、async、const三个关键字, 并且不支持eval和Promise异步。
 
 ● 以下是增强API：
 env() ※返回环境变量对象(包含发言者的QQ信息和群信息等)
-self() ※返回当前群的根数据对象
+self() ※返回当前群的根数据对象()
 alert(msg) ※输出内容到调用的群或私聊(无返回值)
 $.ajax(url, callback, headers=null) ※暂时不支持POST
 ※用法: $.ajax("https://www.example.com", function(data){
@@ -30,7 +30,7 @@ $.ajax(url, callback, headers=null) ※暂时不支持POST
 　　　发送群聊: $.sendGroupMsg(gid,msg)
 　　　撤回消息: $.deleteMsg(message_id)
 　　设置群名片: $.setGroupCard(uid,card)
-　处理加群请求: $.setGroupRequest(flag,approve=true,reason=undefined) ※flag可以在群事件中拿到，reason仅在拒绝时有效
+　处理加群请求: $.setGroupRequest(flag,approve=true,reason=undefined) ※flag可以在群事件中拿到, reason仅在拒绝时有效
 　　发布群公告: $.sendGroupNotice(title,content)
 　　　　群踢人: $.setGroupKick(uid)
 　　　　群禁言: $.setGroupBan(uid,duration=60)
@@ -38,11 +38,11 @@ $.ajax(url, callback, headers=null) ※暂时不支持POST
 　　设置群头衔: $.setGroupSpecialTitle(uid,title,duration=-1)
 　　获取群信息: $.getGroupInfo() ※返回一个包含当前群数据的对象
 更新群信息缓存: $.updateGroupCache()
-※uid表示QQ号，gid表示群号
-※有调用频率限制，部分需要管理员或群主权限
+※uid表示QQ号, gid表示群号
+※有调用频率限制, 部分需要管理员或群主权限
 
 ● 自定义群事件处理：
-需要自行重写"on_message_群号"和"on_notice_群号"函数，例如群号为1234567，则重写
+需要自行重写"on_message_群号"和"on_notice_群号"函数, 例如群号为1234567, 则重写
 
 //该函数在有群消息时触发
 function on_message_1234567(data) { 
@@ -55,9 +55,9 @@ function on_event_1234567(data) {
 }
 
 ● 其他可用函数：
-at(QQ号) ※返回at一个人(string)，默认为发言者
+at(QQ号) ※返回at一个人(string), 默认为发言者
 qq(), qun() ※返回发言者的QQ号,群号(number)
-user(card=1) ※返回发言者的群名片或昵称(string)，card参数为真时优先取群名片
+user(card=1) ※返回发言者的群名片或昵称(string), card参数为真时优先取群名片
 img(url, cache=true) ※返回一张图片(string)
 random(min,max) ※随机返回一个min到max-1的整数(number)
 time2str(timestamp) ※把时间戳转换为一个容易理解的时间(string)
@@ -69,8 +69,8 @@ hmac(algorithm, key, data) ※用法: hmac("md5","secret-key","str")
 querystring ※nodejs的querystring模块
 
 ● 注意
-宿主进程有时会重启，此时环境内所有全局对象都会经历一次序列化和反序列化，
-会导致以下数据会丢失：对象间的继承关系，对象中的成员函数。
+宿主进程有时会重启, 此时环境内所有全局对象都会经历一次序列化和反序列化，
+会导致以下数据会丢失：对象间的继承关系, 对象中的成员函数。
 
 ※欢迎加入小兔子俱乐部(892703008)
 ※20200705`
