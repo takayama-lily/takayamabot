05ba5d5a8cade681c7dd0cf6af1a8d7d6482a8e6
takayama <takayama@foxmail.com>
2020-07-30 01:44:30 +0900

    1

--- modules/sandbox/sandbox.js
+++ modules/sandbox/sandbox.js
@@ -170,6 +170,15 @@ module.exports.run = (code)=>{
     let debug = ["\\","＼"].includes(code.substr(0, 1))
     if (debug)
         code = code.substr(1)
+    code = code.replace(/(（|）|，|″|“|”|＝)/g, (s)=>{
+        if (["″","“","”"].includes(s)) return "\""
+        if (s === "，") return ","
+        if (s === "＝") return "="
+        // if (s === "&amp;") return "&"
+        // if (s === "&#91;") return "["
+        // if (s === "&#93;") return "]"
+        return String.fromCharCode(s.charCodeAt(0) - 65248)
+    })
     try {
         let code2 = vm.runInContext(`this.beforeExec(${JSON.stringify(code)})`, context, {timeout: timeout})
         if (typeof code2 === "string")
